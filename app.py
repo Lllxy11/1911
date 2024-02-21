c9046495bcd4dc8bae0590611be5a74db915ee61
Lllxy11 <160450676+Lllxy11@users.noreply.github.com>
2024-02-21 11:07:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def image_gpt():
 def image_result():
      q = request.form.get("q")
      r=replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-            input={"prompt":q,})
+            input={"prompt":q})
      time.sleep(10)
     return render_template("image_result.html",r=r[0])
 
