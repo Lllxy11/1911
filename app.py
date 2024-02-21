11199a23b992d730cedbc4f9b44b20dd45918cbb
Lllxy11 <160450676+Lllxy11@users.noreply.github.com>
2024-02-21 10:42:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
 from flask import Flask, request, render_template
 import replicate
 import os
+import time
 
 app = Flask(__name__)
-os.environ["REPLICATE_API_TOKEN"] = "r8_YCkY5TTYO4H6Vu2IH9Navg8JezCJBqx3sDOM1"
+os.environ["REPLICATE_API_TOKEN"]="r8_YCkY5TTYO4H6Vu2IH9Navg8JezCJBqx3sDOM1"
+
+r = ""
+first_time = 1
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-@app.route("/main", methods=["POST"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
-    r = request.form.get("r")
+    global r, first_time
+    if first_time == 1:
+        r = request.form.get("r")
+        first_time = 0
     return render_template("main.html", r=r)
 
 @app.route("/image_gpt", methods=["GET", "POST"])
 def image_gpt():
     return render_template("image_gpt.html")
 
-@app.route("/image_result", methods=["POST"])
+@app.route("/image_result", methods=["GET", "POST"])
 def image_result():
-    q = request.form.get("q")
-    try:
-        r = replicate.run(
-            "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-            input={"prompt": q}
-        )
-        # 这里可以根据 replicate.run() 的返回值处理结果
-        return render_template("image_result.html", r=r[0])
-    except Exception as e:
-        # 异常处理，可以返回一个错误页面或者信息
-        return render_template("error.html", error=str(e))
+     q = request.form.get("q")
+     r=replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+            input={"prompt":q,})
+     time.sleep(10)
+    return render_template("image_result.html",r=r[0])
 
 @app.route("/end", methods=["GET", "POST"])
 def end():
+    global first_time
+    first_time = 1
     return render_template("end.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
